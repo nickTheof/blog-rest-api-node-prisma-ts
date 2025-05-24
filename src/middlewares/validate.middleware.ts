4029706c746d1179de88e3c1f329c167b9669602
Nikolaos Theofanis <156532831+nickTheof@users.noreply.github.com>
2025-05-24 23:59:59 +0300

    Feat: add validateBody, validateQuery middleware to use validate the zod schemas in routing

---
+++ src/middlewares/validate.middleware.ts
@@ -0,0 +1,23 @@
+import {NextFunction, Request, Response} from 'express';
+import {z} from "zod";
+
+export const validateBody =
+    (schema: z.Schema) => (req: Request, res: Response, next: NextFunction) => {
+    try {
+        req.body = schema.parse(req.body);
+        next()
+    } catch (err) {
+        next(err)
+    }
+}
+
+export const validateQuery =
+    (schema: z.Schema) => (req: Request, res: Response, next: NextFunction) => {
+    try {
+        res.locals.validatedQuery = schema.parse(req.query);
+        next()
+    } catch (err) {
+        next(err)
+    }
+}
+
