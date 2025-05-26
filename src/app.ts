4eba21ce91fe3f84d3e989d617faf3363d4cbee8
Nikolaos Theofanis <156532831+nickTheof@users.noreply.github.com>
2025-05-26 23:32:12 +0300

    chore: add auth route in the app express

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
-import express from 'express';
+import express, {Express} from 'express';
 import userRoutes from "./routes/user.routes";
+import authRoutes from "./routes/auth.routes";
 import categoryRoutes from "./routes/category.routes";
 import {errorHandler} from "./middlewares/error.middleware";
 
-const app = express();
+const app: Express = express();
 
 app.use(express.json());
+app.use("/api/v1/auth", authRoutes)
 app.use("/api/v1/users", userRoutes)
 app.use("/api/v1/categories", categoryRoutes)
 app.use(errorHandler)
