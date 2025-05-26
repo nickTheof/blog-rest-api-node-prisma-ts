33a3611068e8b7ab65ec1d71b21e5a9f8afb83d9
Nikolaos Theofanis <156532831+nickTheof@users.noreply.github.com>
2025-05-26 23:38:59 +0300

    Feat: add loginDTO, registerDTO for user schema

--- src/schemas/user.schema.ts
+++ src/schemas/user.schema.ts
@@ -31,5 +31,26 @@ export const updateUserSchema = z.object({
     role: z.nativeEnum(Role).optional()
 })
 
-export type CreateUserSchema = z.infer<typeof createUserSchema>;
-export type UpdateUserSchema = z.infer<typeof updateUserSchema>;
+export const loginDTOSchema = z.object({
+    email: z.string().email("Email has not valid format"),
+    password: z.string().min(8, "Password must contain at least 8 characters")
+}).strict("You should provide email and password")
+
+export const registerDTOSchema = z.object({
+    email: z.string().email("Email has not valid format"),
+    password: z.string()
+        .min(8, "Password must contain at least 8 characters")
+        .refine(val => /[a-z]/.test(val), { message: 'Must contain lowercase' })
+        .refine(val => /[A-Z]/.test(val), { message: 'Must contain uppercase' })
+        .refine(val => /\d/.test(val), { message: 'Must contain number' })
+        .refine(val => /[!@#$%^&*]/.test(val), { message: 'Must contain special character' }),
+    confirmPassword: z.string()
+}).strict().superRefine((data, ctx) => {
+    if (data.password !== data.confirmPassword) {
+        ctx.addIssue({
+            path: ['confirmPassword'],
+            message: "Passwords don't match",
+            code: z.ZodIssueCode.custom
+        });
+    }
+});
