bcc202779221030332b5ec7085f0e7d21a715850
Nikolaos Theofanis <156532831+nickTheof@users.noreply.github.com>
2025-05-28 00:48:06 +0300

    feat: implement dynamic pagination in get all users. Use map formatter in user responses

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -3,83 +3,92 @@ import catchAsync from "../utils/catchAsync";
 import userService from "../service/user.service";
 import {CreateUserSchema, UpdateUserSchema, PaginationQuery} from "../types/zod-schemas.types";
 import {AppError} from "../utils/AppError";
+import {formatUsers, sendPaginatedUsersResponse} from "../utils/helpers/response.helpers";
+import {User} from "@prisma/client";
+import {AuthResponse} from "../middlewares/auth.middleware";
+import {UserTokenPayload} from "../types/user-auth.types";
 
 
 const getAllUsers = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
-    const users = await userService.getAll();
-    return res.status(200).json({
-        status: 'success',
-        results: users.length,
-        data: users.map(user => ({
-            ...user,
-            id: user.id.toString(),
-        }))
-    })
-})
-
-const getAllUsersPaginated = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
     const query = res.locals.validatedQuery as PaginationQuery;
-    const users = await userService.getAllPaginated(query);
-    return res.status(200).json({
-        status: 'success',
-        totalItems: await userService.countAll(),
-        totalPages: Math.ceil(users.length / query.limit),
-        currentPage: query.page,
-        limit: query.limit,
-        data: users.map(user => ({
-            ...user,
-            id: user.id.toString(),
-        }))
-    })
+    if (!query.paginated) {
+        const users: User[] = await userService.getAll();
+        return res.status(200).json({
+            status: 'success',
+            results: users.length,
+            data: formatUsers(users)
+        })
+    } else {
+        const [users, totalItems] = await Promise.all([
+            userService.getAllPaginated(query),
+            userService.countAll()
+        ]);
+        return sendPaginatedUsersResponse(res, users, query, totalItems);
+    }
 })
 
 const getUserByUuid = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
     const {uuid} = req.params;
-    const user = await userService.getByUuid(uuid);
+    const user: User | null = await userService.getByUuid(uuid);
     if (!user) {
         return next(new AppError('EntityNotFound', `User with Uuid ${uuid} not found!`));
     }
     return res.status(200).json({
         status: 'success',
-        data: {
-            ...user,
-            id: user.id.toString(),
-        }
+        data: formatUsers([user])[0]
+    })
+})
+
+const getAuthenticatedUser = catchAsync( async(req: Request, res: Response, next: NextFunction) => {
+    const authResponse = res as AuthResponse;
+    const authenticatedUser: UserTokenPayload = authResponse.locals.user;
+    const currentUser: User | null = await userService.getByUuid(authenticatedUser.uuid);
+    if (!currentUser) {
+        return next(new AppError('EntityNotFound', `User with Uuid ${authenticatedUser.uuid} not found!`));
+    }
+    return res.status(200).json({
+        status: 'success',
+        data: formatUsers([currentUser])[0]
     })
 })
 
 const insertUser = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
     const data: CreateUserSchema = req.body;
-    const user = await userService.create(data);
+    const user: User = await userService.create(data);
     return res.status(201).json({
         status: 'success',
-        data: {
-            ...user,
-            id: user.id.toString(),
-        }
+        data: formatUsers([user])[0]
     })
 })
 
 const updateUserByUuid = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
     const {uuid} = req.params;
     const data: UpdateUserSchema = req.body;
-    const toUpdate = await userService.getByUuid(uuid);
+    const toUpdate: User | null = await userService.getByUuid(uuid);
     if (!toUpdate) {
         return next(new AppError('EntityNotFound', `User with Uuid ${uuid} not found!`));
     }
-    const updatedUser = await userService.updateByUuid(uuid, data);
+    const updatedUser: User = await userService.updateByUuid(uuid, data);
     return res.status(200).json({
         status: 'success',
-        data: {
-            ...updatedUser,
-            id: updatedUser.id.toString(),
-        }
+        data: formatUsers([updatedUser])[0]
+    })
+})
+
+const updateAuthenticatedUser = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+    const authResponse = res as AuthResponse;
+    const authenticatedUser: UserTokenPayload = authResponse.locals.user;
+    const data: UpdateUserSchema = req.body;
+    const updatedUser: User = await userService.updateByUuid(authenticatedUser.uuid, data);
+    return res.status(200).json({
+        status: 'success',
+        data: formatUsers([updatedUser])[0]
     })
 })
 
 const deleteUserByUuid = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
     const {uuid} = req.params;
-    const toDelete = await userService.getByUuid(uuid);
+    const toDelete: User | null = await userService.getByUuid(uuid);
     if (!toDelete) {
         return next(new AppError('EntityNotFound', `User with Uuid ${uuid} not found!`));
     }
@@ -90,12 +99,25 @@ const deleteUserByUuid = catchAsync(async (req: Request, res: Response, next: Ne
     })
 })
 
+// Softly delete authenticated user
+const deleteAuthenticatedUser = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+    const authResponse = res as AuthResponse;
+    const authenticatedUser: UserTokenPayload = authResponse.locals.user;
+    await userService.deleteSoftByUuid(authenticatedUser.uuid)
+    return res.status(204).json({
+        status: 'success',
+        data: null
+    })
+})
+
 export default {
     getAllUsers,
-    getAllUsersPaginated,
     getUserByUuid,
+    getAuthenticatedUser,
     insertUser,
     updateUserByUuid,
+    updateAuthenticatedUser,
     deleteUserByUuid,
+    deleteAuthenticatedUser
 }
 
