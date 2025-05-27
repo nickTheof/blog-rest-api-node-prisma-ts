ea454b1c35161b2dfc621689047eb49938384777
Nikolaos Theofanis <156532831+nickTheof@users.noreply.github.com>
2025-05-28 00:53:07 +0300

    feat: mount post, profile routes in application

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import express, {Express} from 'express';
 import userRoutes from "./routes/user.routes";
 import authRoutes from "./routes/auth.routes";
 import categoryRoutes from "./routes/category.routes";
+import postRoutes from "./routes/post.routes";
+import profileRoutes from "./routes/profile.routes";
 import {errorHandler} from "./middlewares/error.middleware";
 
 const app: Express = express();
@@ -10,6 +12,8 @@ app.use(express.json());
 app.use("/api/v1/auth", authRoutes)
 app.use("/api/v1/users", userRoutes)
 app.use("/api/v1/categories", categoryRoutes)
+app.use("/api/v1/posts", postRoutes)
+app.use("/api/v1/profiles", profileRoutes)
 app.use(errorHandler)
 
 export default app;
