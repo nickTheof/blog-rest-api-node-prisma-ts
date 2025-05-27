73115a0035657850eb331e2d20b05703e2e17fa4
Nikolaos Theofanis <156532831+nickTheof@users.noreply.github.com>
2025-05-28 00:52:39 +0300

    feat: add post, profile routes in user route

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -6,24 +6,51 @@ import {validateBody, validateQuery} from "../middlewares/validate.middleware";
 import {createUserSchema, updateUserSchema} from "../schemas/user.schema";
 import {paginationQuerySchema} from "../schemas/pagination-query.schema";
 import {postCreateSchema, postUpdateSchema} from "../schemas/post.schema";
+import {Role} from "@prisma/client";
+import profileController from "../controller/profile.controller";
+import {profileCreateSchema, profileUpdateSchema} from "../schemas/profile.schema";
 
 const router: Router = Router();
 
+// All routes below require authentication
 router.use(verifyToken)
 
-router.post("/me/posts", verifyToken, validateBody(postCreateSchema), postController.insertPost)
+router.route("/me")
+    .get(userController.getAuthenticatedUser)
+    .patch(validateBody(updateUserSchema), userController.updateAuthenticatedUser)
+    .delete(userController.deleteAuthenticatedUser);
 
-// All routes require authentication token. Only users with ADMIN Authorization are eligible for CRUD actions in the User model
-router.use(verifyToken, verifyRoles("ADMIN"));
+router.route("/me/posts")
+    .get(validateQuery(paginationQuerySchema), postController.getAllUserPosts)
+    .post(validateBody(postCreateSchema), postController.insertPost);
+
+
+router.route("/me/posts/:uuid")
+    .get(postController.getAuthenticatedUserPostByUuid)
+    .patch(validateBody(postUpdateSchema), postController.updateAuthenticatedUserPost)
+    .delete(postController.authUserDeletePostByUuid);
+
+router.route("/me/profile")
+    .get(profileController.getAuthenticatedUserProfile)
+    .post(validateBody(profileCreateSchema), profileController.createAuthenticatedUserProfile)
+    .patch(validateBody(profileUpdateSchema), profileController.updateAuthenticatedUserProfile)
+    .delete(profileController.deleteAuthenticatedUserProfile)
+
+// Only ADMIN users can access the following routes
+router.use(verifyRoles(Role.ADMIN));
 router.route('/')
-    .get(userController.getAllUsers)
+    .get(validateQuery(paginationQuerySchema), userController.getAllUsers)
     .post(validateBody(createUserSchema), userController.insertUser)
-
-router.get('/paginated', validateQuery(paginationQuerySchema), userController.getAllUsersPaginated)
 
 router.route('/:uuid')
     .get(userController.getUserByUuid)
     .patch(validateBody(updateUserSchema), userController.updateUserByUuid)
     .delete(userController.deleteUserByUuid)
+
+router.get("/:uuid/posts", validateQuery(paginationQuerySchema), postController.getAllUserPostsByUuid)
+router.route("/:uuid/posts/:postUuid")
+    .get(postController.getPostByUserUuidAndPostUuid)
+    .delete(postController.deletePostByUserUuidAndPostUuid)
+    .patch(validateBody(postUpdateSchema), postController.updatePostByUserUuidAndPostUuid);
 
 export default router;
