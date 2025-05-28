408a8149d9546e27346e6b0b6ce829f5cd24689b
Nikolaos Theofanis <156532831+nickTheof@users.noreply.github.com>
2025-05-28 22:44:53 +0300

    feat: add zod params validation schema

---
+++ src/schemas/params-validation.schema.ts
@@ -0,0 +1,29 @@
+import { z } from 'zod';
+
+export const integerIdParamsSchema = z.object({
+    id: z.string()
+        .refine((val) => /^\d+$/.test(val) && Number.parseInt(val, 10) > 0, {
+            message: "ID must be a positive integer",
+        })
+        .transform((val) => parseInt(val, 10)),
+});
+
+export const uuidParamsSchema = z.object({
+    uuid: z.string()
+        .uuid("Invalid UUID format. Use 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx' format.")
+})
+
+export const bigintIdParamSchema = z.object({
+    id: z.string()
+        .refine((val) => /^\d+$/.test(val) && BigInt(val) > 0n, {
+            message: "ID must be a positive bigint",
+        })
+        .transform((val) => BigInt(val)),
+});
+
+export const doubleUuidParamsSchema = z.object({
+    uuid: z.string()
+        .uuid("Invalid UUID format. Use 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx' format."),
+    postUuid: z.string()
+        .uuid("Invalid UUID format. Use 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx' format."),
+});
