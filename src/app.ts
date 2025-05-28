b6e21258fbb1fb8511ad696b5b24304ba6e101ad
Nikolaos Theofanis <156532831+nickTheof@users.noreply.github.com>
2025-05-28 22:42:30 +0300

    feat: add rate-limiter, cors, hpp middlewares, add a route to handle all the unimplemented routes

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,58 @@
 import express, {Express} from 'express';
+import helmet from "helmet";
+import cors from "cors";
+import hpp from "hpp";
 import userRoutes from "./routes/user.routes";
 import authRoutes from "./routes/auth.routes";
 import categoryRoutes from "./routes/category.routes";
 import postRoutes from "./routes/post.routes";
 import profileRoutes from "./routes/profile.routes";
 import {errorHandler} from "./middlewares/error.middleware";
+import {AppError} from "./utils/AppError";
+import config from "./config/config";
+import limiter from "./middlewares/limiter.middleware";
 
 const app: Express = express();
 
-app.use(express.json());
+// Use helmet for setting security headers
+app.use(
+    helmet({
+        contentSecurityPolicy: false,
+    })
+);
+
+// Configure the allowed origins for this backend API
+const ALLOWED_ORIGINS = config.ALLOWED_ORIGINS.split(",") || [];
+app.use(
+    cors({
+        origin: ALLOWED_ORIGINS,
+        credentials: true,
+    })
+);
+
+app.use(express.json({ limit: "10kb" }));
+app.use(express.urlencoded({ extended: true, limit: "10kb" }));
+
+app.use(
+    hpp({
+        whitelist: [
+            "paginated",
+            "page",
+            "limit"
+        ],
+    })
+);
+
+app.use("/", limiter);
 app.use("/api/v1/auth", authRoutes)
 app.use("/api/v1/users", userRoutes)
 app.use("/api/v1/categories", categoryRoutes)
 app.use("/api/v1/posts", postRoutes)
 app.use("/api/v1/profiles", profileRoutes)
+
+app.all("/{*splat}", (req, res, next) => {
+    next(new AppError(`EntityNotFound`, "Can't find the ${req.originalUrl} on the server"));
+});
 app.use(errorHandler)
 
 export default app;
