777004e071775b5edcdd08eedb10ccf01a2916c4
Nikolaos Theofanis <156532831+nickTheof@users.noreply.github.com>
2025-05-29 17:38:02 +0300

    refactor: change the published field to status in zod PostSchemas

--- src/schemas/post.schema.ts
+++ src/schemas/post.schema.ts
@@ -1,9 +1,10 @@
 import { z } from 'zod';
+import {PostStatus} from "@prisma/client";
 
 export const postCreateSchema = z.object({
     title: z.string().min(2, "Title must contain at least 2 characters"),
     description: z.string().min(2, "Description must contain at least 2 characters"),
-    published: z.boolean().optional(),
+    status: z.nativeEnum(PostStatus).default(PostStatus.DRAFT).optional(),
     categories: z.array(z.number()).optional(),
 
 })
