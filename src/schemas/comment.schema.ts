285823a0ce9a4ab6b9c3dec2c65b3c50b917d69e
Nikolaos Theofanis <156532831+nickTheof@users.noreply.github.com>
2025-05-30 13:38:35 +0300

    fix: add the status field in the schema of the commentCreate and commentUpdate

--- src/schemas/comment.schema.ts
+++ src/schemas/comment.schema.ts
@@ -1,7 +1,9 @@
 import { z } from 'zod';
+import {CommentStatus} from "@prisma/client";
 
 export const commentCreateSchema = z.object({
     title: z.string().min(2, "Comment must contain at least 2 characters"),
+    status: z.nativeEnum(CommentStatus).default(CommentStatus.ACTIVE).optional(),
 })
 
 export const commentUpdateSchema = commentCreateSchema.partial();
