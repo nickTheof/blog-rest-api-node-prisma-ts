7bc318f655e859f031ab6f2d206f85d81b3766df
Nikolaos Theofanis <156532831+nickTheof@users.noreply.github.com>
2025-05-30 13:47:12 +0300

    refactor: use the filterCommentsPaginationQuery instead of PaginationQuery

--- src/routes/comment.routes.ts
+++ src/routes/comment.routes.ts
@@ -1,6 +1,6 @@
 import {Router} from 'express';
 import {validateBody, validateParams, validateQuery} from "../middlewares/validate.middleware";
-import {paginationQuerySchema} from "../schemas/pagination-query.schema";
+import {filterCommentsPaginationQuerySchema, paginationQuerySchema} from "../schemas/pagination-query.schema";
 import commentController from "../controller/comment.controller";
 import {verifyRoles, verifyToken } from "../middlewares/auth.middleware";
 import {Role} from "@prisma/client";
@@ -10,13 +10,13 @@ import {commentUpdateSchema} from "../schemas/comment.schema";
 const router: Router = Router();
 
 router.use(verifyToken, verifyRoles(Role.ADMIN))
-router.get("/", validateQuery(paginationQuerySchema), commentController.getAllComments)
+router.get("/", validateQuery(filterCommentsPaginationQuerySchema), commentController.getAllComments)
 router.route("/:uuid")
     .get(validateParams(uuidParamsSchema), commentController.getCommentByUuid)
     .delete(validateParams(uuidParamsSchema), commentController.deleteCommentByUuid)
     .patch(validateParams(uuidParamsSchema), validateBody(commentUpdateSchema), commentController.updateCommentByUuid);
 
-router.get("/post/:uuid", validateParams(uuidParamsSchema), validateQuery(paginationQuerySchema), commentController.getAllCommentsByPostUuid)
-router.get("/user/:uuid", validateParams(uuidParamsSchema), validateQuery(paginationQuerySchema), commentController.getAllCommentsByUserUuid)
+router.get("/post/:uuid", validateParams(uuidParamsSchema), validateQuery(filterCommentsPaginationQuerySchema), commentController.getAllCommentsByPostUuid)
+router.get("/user/:uuid", validateParams(uuidParamsSchema), validateQuery(filterCommentsPaginationQuerySchema), commentController.getAllCommentsByUserUuid)
 
 export default router;
